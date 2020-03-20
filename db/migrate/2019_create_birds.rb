b5af846cc621fa6dc8a899eed110b7dbf5b63601
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-19 21:50:07 -0400

    Done.

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,1 +1,13 @@
-# Add code from Readme
+# Add code from Readmelass CreateBirds < ActiveRecord::Migration[5.2]
+  # db/migrate/2019_create_birds.rb
+
+class CreateBirds < ActiveRecord::Migration[5.2]
+    def change
+      create_table :birds do |t|
+        t.string :name
+        t.string :species
+  
+        t.timestamps
+      end
+    end
+  end
